bb61b3eb94969ee4904bad83f14341c53285952a
Imam <imam@Ibees-MacBook-Air.local>
2024-10-15 16:54:36 +0530

    showing product item based on category

--- src/main/java/com/imam/controller/HomeController.java
+++ src/main/java/com/imam/controller/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -36,16 +38,21 @@ public class HomeController {
 	}
 	
 	@GetMapping("/products")
-	public String products(Model model) {
+	public String products(Model model, @RequestParam(value = "category", defaultValue = "") String category) {
+		System.out.println("category"+category);
 		List<Category> categories= categoryService.getAllActiveCategory();
-		List<Product> products= productService.getAllActiveProducts();
+		List<Product> products= productService.getAllActiveProducts(category);
+		System.out.println(products);
 		model.addAttribute("categories",categories);
 		model.addAttribute("products",products);
+		model.addAttribute("paramValue",category);
 		return "product";
 	}
 	
-	@GetMapping("/product")
-	public String product() {
+	@GetMapping("/product/{id}")
+	public String product(@PathVariable int id, Model model) {
+		Product product=productService.getProductById(id);
+		model.addAttribute("product",product);
 		return "view_product";
 	}
 
--- src/main/java/com/imam/repository/ProductRepository.java
+++ src/main/java/com/imam/repository/ProductRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
     public List<Product> findByIsActiveTrue();
+
+    public List<Product> findByCategory(String category);
 }
